5f532a71d6dc0ae20b8206ed56eb2daf38fd1101
lashevskiy <lashevskiy@gmail.com>
2022-06-13 17:33:51 +0300

    Updates

---
+++ precache-manifest.07ab67396b76734a97c0ecccba3f3d0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204fc74014b67f7fa8db346f449a352d",
+    "url": "/RepetitRu/index.html"
+  },
+  {
+    "revision": "d0114a525dedbbe4cf91",
+    "url": "/RepetitRu/static/css/2.f53a4a22.chunk.css"
+  },
+  {
+    "revision": "aab349fedc7777a1befd",
+    "url": "/RepetitRu/static/css/main.5d8b81b4.chunk.css"
+  },
+  {
+    "revision": "d0114a525dedbbe4cf91",
+    "url": "/RepetitRu/static/js/2.4e595ef0.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/RepetitRu/static/js/2.4e595ef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab349fedc7777a1befd",
+    "url": "/RepetitRu/static/js/main.0f44cf28.chunk.js"
+  },
+  {
+    "revision": "03b7997a1e2fc33ef4e4",
+    "url": "/RepetitRu/static/js/runtime-main.133af1e0.js"
+  }
+]);
